f8c7c348189f6582f5a316303e00bdc94b2f75e8
Suprio-Das <priyamd507@gmail.com>
2025-08-25 23:09:36 +0600

    User schema created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import DbConn from './utils/db.js';
 dotenv.config();
 
 // Declaring Port
@@ -12,6 +13,9 @@ const app = express();
 // Express and CORS Middlewares
 app.use(express.json())
 app.use(cors())
+
+// Database Connection Function
+DbConn();
 
 // Testing Route
 app.get('/', (req, res) => {
