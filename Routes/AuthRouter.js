325564b69f52f844a3831c3175276063a45c46a2
Suprio-Das <priyamd507@gmail.com>
2025-08-26 22:26:50 +0600

    Login route tested

--- Routes/AuthRouter.js
+++ Routes/AuthRouter.js
@@ -4,5 +4,8 @@ import { register } from '../Controllers/AuthControllers.js';
 const AuthRoutes = express.Router();
 
 AuthRoutes.post('/register', register)
+AuthRoutes.post('/login', (req, res) => {
+    console.log("Login is working");
+})
 
 export default AuthRoutes;
