f8c884656cda86917df319e82c37627d4305e3c4
Suprio-Das <priyamd507@gmail.com>
2025-08-27 21:24:39 +0600

    jwt token decoded and search for the user

--- Controllers/UserControllers.js
+++ Controllers/UserControllers.js
@@ -1,3 +1,6 @@
+import jwt from 'jsonwebtoken'
+import UserModel from '../Models/User';
+
 export const createNote = async (req, res) => {
     try {
         const { name, description } = req.body;
@@ -10,6 +13,12 @@ export const createNote = async (req, res) => {
         if (!token) {
             return res.status(401).json({ success: false, message: 'Unauthorized user.' })
         }
+
+        const decoded_token = jwt.verify(token, process.env.JWT_Secret)
+
+        const user = await UserModel.findById(decoded_token.userId);
+
+
     } catch (error) {
         res.json({ success: false, message: error });
     }
