2a2e91a7aa06cad16658ea39743c9c28dab5a74d
Suprio-Das <priyamd507@gmail.com>
2025-08-27 21:08:12 +0600

    logout route created

--- Routes/AuthRouter.js
+++ Routes/AuthRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { login, register } from '../Controllers/AuthControllers.js';
+import { login, logout, register } from '../Controllers/AuthControllers.js';
 
 const AuthRoutes = express.Router();
 
 AuthRoutes.post('/register', register)
 AuthRoutes.post('/login', login)
+AuthRoutes.post('/logout', logout)
 
 export default AuthRoutes;
