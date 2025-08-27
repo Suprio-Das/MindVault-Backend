66fdad7c347701e0a77679f827918ff78347cc5a
Suprio-Das <priyamd507@gmail.com>
2025-08-27 21:28:43 +0600

    new note model created and data sent to server

--- Controllers/UserControllers.js
+++ Controllers/UserControllers.js
@@ -1,23 +1,35 @@
 import jwt from 'jsonwebtoken'
 import UserModel from '../Models/User';
+import NoteModel from '../Models/Note';
 
 export const createNote = async (req, res) => {
     try {
         const { name, description } = req.body;
         if (!name || !description) {
-            return res.status(400).json({ success: false, message: 'All Fields are required' })
+            res.status(400).json({ success: false, message: 'All Fields are required' })
         }
 
         const token = res.cookies.token;
 
         if (!token) {
-            return res.status(401).json({ success: false, message: 'Unauthorized user.' })
+            res.status(401).json({ success: false, message: 'Unauthorized user.' })
         }
 
         const decoded_token = jwt.verify(token, process.env.JWT_Secret)
 
         const user = await UserModel.findById(decoded_token.userId);
 
+        if (!user) {
+            res.status(401).json({ success: false, message: 'Unauthorized. Operation closed' })
+        }
+
+        const newNote = new NoteModel({
+            name, description, userId
+        })
+
+        newNote.save();
+
+        res.status(200).json({ success: true, message: 'New note created.' })
 
     } catch (error) {
         res.json({ success: false, message: error });
--- Models/Note.js
+++ Models/Note.js
@@ -8,6 +8,10 @@ const NoteSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true
+    },
+    userId: {
+        type: String,
+        required: true
     }
 }, { timestamps: true })
 
