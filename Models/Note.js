f56a52ce648464c294af278c2a2ec97fc88a9ac3
Suprio-Das <priyamd507@gmail.com>
2025-08-27 21:13:47 +0600

    NoteModel and Schema created

---
+++ Models/Note.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const NoteSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+const NoteModel = mongoose.model("notes", NoteSchema);
+export default NoteModel;
