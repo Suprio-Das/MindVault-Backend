cad5d22fbccf741841b7ebb254f7a517b27f734c
Suprio-Das <priyamd507@gmail.com>
2025-09-02 22:31:23 +0600

    GetUsers function completed

--- Controllers/AdminControllers.js
+++ Controllers/AdminControllers.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import UserModel from '../Models/User.js';
 
 export const GetUsers = async (req, res) => {
     try {
-        console.log(req.body.userId)
+        const users = await UserModel.find();
+        res.status(200).json(users)
     } catch (error) {
         res.send(error);
     }
