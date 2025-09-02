3230268e10cf3c185763cfdfdaa9066f1ea4afde
Suprio-Das <priyamd507@gmail.com>
2025-09-02 22:13:19 +0600

    Admin getuser route created

--- Routes/AdminRouter.js
+++ Routes/AdminRouter.js
@@ -1,5 +1,9 @@
 import express from 'express'
+import { isAdmin } from '../Middlewares/verifyToken';
+import { GetUsers } from '../Controllers/AdminControllers';
 
 const AdminRoutes = express.Router();
+
+AdminRoutes.get('/users', isAdmin, GetUsers);
 
 export default AdminRoutes;
